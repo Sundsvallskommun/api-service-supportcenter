0d3735d4ad404db4ad386e1b48983da179723f39
Joakim Schürer <joakim.schurer@sundsvall.se>
2023-03-01 13:20:02 +0100

    UF-4484: Adjusted circuit breaker settings
    - Removed circuitbreaker annotation on feign client
    - Updated Dept44 to version 1.30

--- src/main/java/se/sundsvall/supportcenter/integration/pob/POBClient.java
+++ src/main/java/se/sundsvall/supportcenter/integration/pob/POBClient.java
@@ -1,9 +1,11 @@
 package se.sundsvall.supportcenter.integration.pob;
 
-import generated.client.pob.PobPayload;
-import generated.client.pob.PobPayloadWithTriggerResults;
-import generated.client.pob.SuspensionInfo;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static se.sundsvall.supportcenter.integration.pob.configuration.POBConfiguration.CLIENT_ID;
+
+import java.util.List;
+
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,16 +15,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+
+import generated.client.pob.PobPayload;
+import generated.client.pob.PobPayloadWithTriggerResults;
+import generated.client.pob.SuspensionInfo;
 import se.sundsvall.supportcenter.integration.pob.configuration.POBConfiguration;
 
-import java.util.List;
-
-import static org.springframework.http.HttpHeaders.AUTHORIZATION;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import static se.sundsvall.supportcenter.integration.pob.configuration.POBConfiguration.CLIENT_ID;
-
 @FeignClient(name = CLIENT_ID, url = "${integration.pob.url}", configuration = POBConfiguration.class)
-@CircuitBreaker(name = CLIENT_ID)
 public interface POBClient {
 
 	/**
