2eccac8172b8f60b3acec16bb339f63410da2380
AndreSoderlund <andre.soderlund@b3.se>
2025-05-20 13:03:36 +0200

    Added business logic for creating new assets that have a lease status. Added logic for updating existing assets with a new lease status. Added logic for fetching assets and showing their lease status. Updated unit tests.

--- src/test/java/se/sundsvall/supportcenter/service/mapper/ConfigurationMapperTest.java
+++ src/test/java/se/sundsvall/supportcenter/service/mapper/ConfigurationMapperTest.java
@@ -1,6 +1,5 @@
 package se.sundsvall.supportcenter.service.mapper;
 
-import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import generated.client.pob.PobMemo;
@@ -11,7 +10,6 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
-import se.sundsvall.supportcenter.api.model.Asset;
 import se.sundsvall.supportcenter.api.model.Note;
 import se.sundsvall.supportcenter.api.model.UpdateAssetRequest;
 import se.sundsvall.supportcenter.api.model.enums.NoteType;
@@ -114,7 +112,8 @@ class ConfigurationMapperTest {
 			.withNote(note)
 			.withSupplierStatus("supplierStatus")
 			.withDeliveryDate(deliveryDate)
-			.withWarrantyEndDate(warrantyEndDate);
+			.withWarrantyEndDate(warrantyEndDate)
+			.withLeaseStatus("status1");
 
 		final var assetId = "assetId";
 
@@ -129,6 +128,7 @@ class ConfigurationMapperTest {
 		assertThat(result.getData()).containsEntry("Virtual.LeverantorensStatus", "supplierStatus");
 		assertThat(result.getData()).containsEntry("DeliveryDate", deliveryDate.format(DATE_FORMATTER));
 		assertThat(result.getData()).containsEntry("EndWarrantyDate", warrantyEndDate.format(DATE_FORMATTER));
+		assertThat(result.getData()).containsEntry("Virtual.LeaseStatus", "status1");
 		assertThat(result.getMemo()).extractingByKey("CILeverantorensAnteckningar").extracting(
 			PobMemo::getExtension,
 			PobMemo::getHandleSeparators,
@@ -170,68 +170,5 @@ class ConfigurationMapperTest {
 		assertThat(result.getData()).containsEntry("DeliveryDate", deliveryDate.format(DATE_FORMATTER));
 		assertThat(result.getData()).containsEntry("EndWarrantyDate", warrantyEndDate.format(DATE_FORMATTER));
 		assertThat(result.getMemo()).isEmpty();
-	}
-
-	@Test
-	void toAssetList() {
-
-		// Parameter values
-		final var pobPayloadList = List.of(
-			new PobPayload()
-				.type("ConfigurationItem")
-				.data(Map.of(
-					"Id", "111",
-					"Description", "hardwareDescription-1",
-					"OptionalNumber", "hardwareName-1",
-					"SerialNumber", "serialNumber-1",
-					"CiStatus", "hardwareStatus-1",
-					"Virtual.BluetoothAddress", "macAddress-1",
-					"Virtual.LeverantorensStatus", "supplierStatus-1")),
-			new PobPayload()
-				.type("ConfigurationItem")
-				.data(Map.of(
-					"Id", "222",
-					"Description", "hardwareDescription-2",
-					"OptionalNumber", "hardwareName-2",
-					"SerialNumber", "serialNumber-2",
-					"CiStatus", "hardwareStatus-2",
-					"Virtual.BluetoothAddress", "macAddress-2",
-					"Virtual.LeverantorensStatus", "supplierStatus-2")),
-			new PobPayload()
-				.type("SomethingElseThanConfigurationItem")
-				.data(Map.of(
-					"Id", "333",
-					"Description", "hardwareDescription-3",
-					"OptionalNumber", "hardwareName-3",
-					"SerialNumber", "serialNumber-3",
-					"CiStatus", "hardwareStatus-3",
-					"Virtual.BluetoothAddress", "macAddress-3",
-					"Virtual.LeverantorensStatus", "supplierStatus-3")));
-
-		// Call
-		final var result = ConfigurationMapper.toAssetList(pobPayloadList);
-
-		// Verification
-		assertThat(result).hasSize(3);
-		assertThat(result).extracting(Asset::getId).containsExactly("111", "222", "333");
-		assertThat(result).extracting(Asset::getHardwareDescription).containsExactly("hardwareDescription-1", "hardwareDescription-2", "hardwareDescription-3");
-		assertThat(result).extracting(Asset::getHardwareName).containsExactly("hardwareName-1", "hardwareName-2", "hardwareName-3");
-		assertThat(result).extracting(Asset::getSerialNumber).containsExactly("serialNumber-1", "serialNumber-2", "serialNumber-3");
-		assertThat(result).extracting(Asset::getHardwareStatus).containsExactly("hardwareStatus-1", "hardwareStatus-2", "hardwareStatus-3");
-		assertThat(result).extracting(Asset::getMacAddress).containsExactly("macAddress-1", "macAddress-2", "macAddress-3");
-		assertThat(result).extracting(Asset::getSupplierStatus).containsExactly("supplierStatus-1", "supplierStatus-2", "supplierStatus-3");
-	}
-
-	@Test
-	void toAssetListNoResults() {
-
-		// Parameter values
-		final List<PobPayload> pobPayloadList = emptyList();
-
-		// Call
-		final var result = ConfigurationMapper.toAssetList(pobPayloadList);
-
-		// Verification
-		assertThat(result).isNotNull().isEmpty();
 	}
 }
